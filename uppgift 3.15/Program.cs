f7cc913b8e48a024ec7268c1bb491819c48b2634
46703 <46703@DESKTOP-FIH4LKA>
2022-10-04 18:02:27 +0200

    Add project files.

---
+++ uppgift 3.15/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace inlämningsuppgift 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur många timmar vill du hyra bilen?");
+            int hej = int.Parse(Console.ReadLine());
+            int pris = hej * 80;
+            
+
+            if (pris > 950)
+            {
+                Console.WriteLine("Du får inte hyra bilen");
+
+            }
+            else
+            {
+                Console.WriteLine("Du får låna bilen, det blir " + pris + " kronor");
+
+            }
+        }
+    }
+}
